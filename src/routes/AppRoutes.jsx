ab4cb7ccb3c9e5b6e288b0c68c88109b58238f28
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-08 23:19:10 +0100

    Rota de telemedicina adiconada

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,6 +4,7 @@ import Servicos from "../pages/site/Servicos";
 import Sobre from "../pages/site/Sobre";
 import Especialistas from "../pages/site/Especialistas";
 import Agendar from "../pages/site/Agendar";
+import Telemedicina from "../pages/site/Telemedicina";
 import NotFound from "../pages/site/NotFound";
 
 export default function AppRoutes () {
@@ -15,6 +16,7 @@ export default function AppRoutes () {
             <Route path="/sobre" element={<Sobre />} />
             <Route path="/especialistas" element={<Especialistas />} />
             <Route path="/agendar" element={<Agendar />} />
+            <Route path="/telemedicina" element={<Telemedicina />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     )
