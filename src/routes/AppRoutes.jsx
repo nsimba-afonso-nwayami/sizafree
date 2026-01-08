51b77ae0e8d32952ff56e5c2f36640169e752891
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-08 15:52:09 +0100

    Página especialistas adicionadas nas rotas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/site/Home";
 import Servicos from "../pages/site/Servicos";
 import Sobre from "../pages/site/Sobre";
+import Especialistas from "../pages/site/Especialistas";
 import NotFound from "../pages/site/NotFound";
 
 export default function AppRoutes () {
@@ -11,6 +12,7 @@ export default function AppRoutes () {
             <Route path="/" element={<Home />} />
             <Route path="/servicos" element={<Servicos />} />
             <Route path="/sobre" element={<Sobre />} />
+            <Route path="/especialistas" element={<Especialistas />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     )
