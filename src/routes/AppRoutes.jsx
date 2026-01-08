a39a2c38edb553c5f4d4465b57d1c5e199434aa1
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-08 17:08:38 +0100

    Rota de agendar adicionada

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import Home from "../pages/site/Home";
 import Servicos from "../pages/site/Servicos";
 import Sobre from "../pages/site/Sobre";
 import Especialistas from "../pages/site/Especialistas";
+import Agendar from "../pages/site/Agendar";
 import NotFound from "../pages/site/NotFound";
 
 export default function AppRoutes () {
@@ -13,6 +14,7 @@ export default function AppRoutes () {
             <Route path="/servicos" element={<Servicos />} />
             <Route path="/sobre" element={<Sobre />} />
             <Route path="/especialistas" element={<Especialistas />} />
+            <Route path="/agendar" element={<Agendar />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     )
