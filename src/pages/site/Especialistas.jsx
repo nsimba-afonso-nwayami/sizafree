80f811173e4f9deffc95676deb31f2931255740b
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-08 16:16:46 +0100

    Página especialistas criada

---
+++ src/pages/site/Especialistas.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+
+export default function Especialistas() {
+  return (
+    <>
+      <title>Especialistas | SizaFree</title>
+
+      {/*Header*/}
+      <Header />
+
+      {/*Footer*/}
+      <Footer />
+    </>
+  );
+}
