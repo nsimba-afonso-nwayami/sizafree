97c5528d5c14953eb0fa1fa9380d1f5742848d6f
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-12 16:17:19 +0100

    Página de notificações do paciente criada

---
+++ src/pages/paciente/NotificacoesPaciente.jsx
@@ -0,0 +1,6 @@
+import { Link } from "react-router-dom";
+import PacienteLayout from "./components/PacienteLayout";
+
+export default function NotificacoesPaciente() {
+  return <PacienteLayout title="Notificações"></PacienteLayout>;
+}
