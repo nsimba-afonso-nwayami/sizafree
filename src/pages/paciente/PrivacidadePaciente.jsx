60470a0b672a6a132faa14e2656f2f028b161b99
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-12 14:52:27 +0100

    Página privacidade do paciente criada

---
+++ src/pages/paciente/PrivacidadePaciente.jsx
@@ -0,0 +1,6 @@
+import { Link } from "react-router-dom";
+import PacienteLayout from "./components/PacienteLayout";
+
+export default function PrivacidadePaciente() {
+  return <PacienteLayout title="Privacidade do Paciente"></PacienteLayout>;
+}
