d6b76d5189c6818ddff1c989da78feb5af98f700
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-12 14:51:33 +0100

    Página histórico do paciente criada

---
+++ src/pages/paciente/HistoricoPaciente.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+import PacienteLayout from "./components/PacienteLayout";
+
+export default function HistoricoPaciente() {
+
+  return (
+    <PacienteLayout title="Histórico do Paciente">
+      
+    </PacienteLayout>
+  );
+}
