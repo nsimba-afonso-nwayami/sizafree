dbef2dcc26114cd31020076f5896597c1e860b69
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-13 09:06:52 +0100

    Página paciente do especialista criada

---
+++ src/pages/especialista/PacientesEspecialista.jsx
@@ -0,0 +1,12 @@
+import EspecialistaLayout from "./components/EspecialistaLayout";
+import { Link } from "react-router-dom";
+
+export default function PacientesEspecialista() {
+  return (
+    <>
+      <title>Pacientes | Dashboard Especialista</title>
+
+      <EspecialistaLayout title="Pacientes"></EspecialistaLayout>
+    </>
+  );
+}
