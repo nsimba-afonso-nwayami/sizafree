a04ed665c19b931c5b520ce3ece90b05c285dc58
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-13 15:46:05 +0100

    Página pacientes hospital criada

---
+++ src/pages/hospital/PacientesHospital.jsx
@@ -0,0 +1,14 @@
+import HospitalLayout from "./components/HospitalLayout";
+import { Link } from "react-router-dom";
+
+export default function PacientesHospital() {
+ 
+  return (
+    <>
+      <title>Pacientes | Dashboard Hospital</title>
+      <HospitalLayout title="Pacientes">
+        
+      </HospitalLayout>
+    </>
+  );
+}
