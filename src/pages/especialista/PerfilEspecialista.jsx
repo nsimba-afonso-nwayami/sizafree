140cd3d82d46e880ba99ae379216d7caf9faa652
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-13 08:17:16 +0100

    Perfil do escialista criado

---
+++ src/pages/especialista/PerfilEspecialista.jsx
@@ -0,0 +1,12 @@
+import EspecialistaLayout from "./components/EspecialistaLayout";
+import { Link } from "react-router-dom";
+
+export default function PerfilEspecialista() {
+  return (
+    <EspecialistaLayout title="Página não encontrada">
+      <>
+        <title>Perfil | Dashboard Especialista</title>
+      </>
+    </EspecialistaLayout>
+  );
+}
