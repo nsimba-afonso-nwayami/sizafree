1d68727c4d42fed58faf0d5e68cfa05af7f818e1
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-13 15:33:31 +0100

    Dashboard do hospital criado

---
+++ src/pages/hospital/DashboardHospital.jsx
@@ -0,0 +1,12 @@
+import HospitalLayout from "./components/HospitalLayout";
+import { Link } from "react-router-dom";
+
+export default function DashboardHospital() {
+  return (
+    <>
+      <title>Dashboard Hospital</title>
+      <HospitalLayout title="Dashboard do Hospital">
+      </HospitalLayout>
+    </>
+  );
+}
