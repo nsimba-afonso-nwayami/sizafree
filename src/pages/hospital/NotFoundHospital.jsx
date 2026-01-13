e2defe1f058d411cccbcb9a4749444503d8c710d
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-13 15:26:55 +0100

    Página notfound do hospital criado

---
+++ src/pages/hospital/NotFoundHospital.jsx
@@ -0,0 +1,12 @@
+import HospitalLayout from "./components/HospitalLayout";
+import { Link } from "react-router-dom";
+
+export default function NotFoundHospital() {
+  return (
+    <>
+      <title>Dashboard Hospital</title>
+      <HospitalLayout title="Dashboard do Hospital">
+      </HospitalLayout>
+    </>
+  );
+}
