4097e42a4cae5fb2e05cd43f57252fd51e8b623b
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-13 09:06:04 +0100

    Página histórico do especialista criada

---
+++ src/pages/especialista/HistoricoEspecialista.jsx
@@ -0,0 +1,12 @@
+import EspecialistaLayout from "./components/EspecialistaLayout";
+import { Link } from "react-router-dom";
+
+export default function HistoricoEspecialista() {
+  return (
+    <>
+      <title>Histórico | Dashboard Especialista</title>
+
+      <EspecialistaLayout title="Histórico"></EspecialistaLayout>
+    </>
+  );
+}
