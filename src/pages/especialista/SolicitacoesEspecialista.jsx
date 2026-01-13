297f73736e02959edc413950d0ba65f88b6d51d9
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-13 09:07:24 +0100

    Página solicitações do especialista criada

---
+++ src/pages/especialista/SolicitacoesEspecialista.jsx
@@ -0,0 +1,14 @@
+import EspecialistaLayout from "./components/EspecialistaLayout";
+import { Link } from "react-router-dom";
+
+export default function SolicitacoesEspecialista() {
+  return (
+    <>
+      <title>Solicitações | Dashboard Especialista</title>
+
+      <EspecialistaLayout title="Solicitações">
+        
+      </EspecialistaLayout>
+    </>
+  );
+}
