cc74817e5256d29d1131bbccd9803bcaf9ce327b
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-13 09:06:40 +0100

    Página notificações do especialista criada

---
+++ src/pages/especialista/NotificacoesEspecialista.jsx
@@ -0,0 +1,12 @@
+import EspecialistaLayout from "./components/EspecialistaLayout";
+import { Link } from "react-router-dom";
+
+export default function NotificacoesEspecialista() {
+  return (
+    <>
+      <title>Notificações | Dashboard Especialista</title>
+
+      <EspecialistaLayout title="Notificações"></EspecialistaLayout>
+    </>
+  );
+}
