e287f21b8e009edb178e1193763056695be9b850
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-13 07:45:54 +0100

    Página not found especialista criada

---
+++ src/pages/especialista/NotFoundEspecialista.jsx
@@ -0,0 +1,12 @@
+import EspecialistaLayout from "./components/EspecialistaLayout";
+import { Link } from "react-router-dom";
+
+export default function NotFoundEspecialista() {
+  return (
+    <EspecialistaLayout title="Página não encontrada">
+      <>
+        <title>Página não encontrada | Dashboard Especialista</title>
+      </>
+    </EspecialistaLayout>
+  );
+}
