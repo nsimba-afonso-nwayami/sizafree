51def98d24f3f7a6bac302ffb10ec08af9a07f58
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-13 16:03:38 +0100

    Página histórico do hospital criada

---
+++ src/pages/hospital/HistoricoHospital.jsx
@@ -0,0 +1,13 @@
+import HospitalLayout from "./components/HospitalLayout";
+
+export default function HistoricoHospital() {
+
+  return (
+    <>
+      <title>Histórico | Dashboard Hospital</title>
+      <HospitalLayout title="Histórico Cliníco">
+        
+      </HospitalLayout>
+    </>
+  );
+}
