3528659b02630709e992a107b4dc4fcc332b8d3e
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-13 09:05:50 +0100

    Página agenda do especialista criada

---
+++ src/pages/especialista/AgendaEspecialista.jsx
@@ -0,0 +1,12 @@
+import EspecialistaLayout from "./components/EspecialistaLayout";
+import { Link } from "react-router-dom";
+
+export default function AgendaEspecialista() {
+  return (
+    <>
+      <title>Agenda | Dashboard Especialista</title>
+
+      <EspecialistaLayout title="Agenda"></EspecialistaLayout>
+    </>
+  );
+}
