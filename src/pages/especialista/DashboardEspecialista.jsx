c644b2eb9b90c141a52a09eb1d62b6386e3c304f
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-13 07:19:53 +0100

    Dashboard do especialista criado

---
+++ src/pages/especialista/DashboardEspecialista.jsx
@@ -0,0 +1,12 @@
+import EspecialistaLayout from "./components/EspecialistaLayout";
+import { Link } from "react-router-dom";
+
+export default function DashboardEspecialista() {
+  return (
+    <EspecialistaLayout title="Dashboard do Especialista">
+      <>
+        <title>Dashboard Especialista</title>
+      </>
+    </EspecialistaLayout>
+  );
+}
