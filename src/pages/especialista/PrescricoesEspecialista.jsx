ce423487cc09146caddfbca115741596c5c8c0c0
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-13 09:07:07 +0100

    Página prescrições do especialista criada

---
+++ src/pages/especialista/PrescricoesEspecialista.jsx
@@ -0,0 +1,12 @@
+import EspecialistaLayout from "./components/EspecialistaLayout";
+import { Link } from "react-router-dom";
+
+export default function PrescricoesEspecialista() {
+  return (
+    <>
+      <title>Prescrições | Dashboard Especialista</title>
+
+      <EspecialistaLayout title="Prescrições"></EspecialistaLayout>
+    </>
+  );
+}
