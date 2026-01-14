403613225b1dfa0bf8b83d823d2d07212c169294
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 15:38:48 +0100

    Página de estoque da farmácia criada

---
+++ src/pages/farmacia/EstoqueFarmacia.jsx
@@ -0,0 +1,11 @@
+import FarmaciaLayout from "./components/FarmaciaLayout";
+
+export default function EstoqueFarmacia() {
+  return (
+    <>
+      <title>Estoque | Dashboard Farmácia</title>
+
+      <FarmaciaLayout title="Estoque"></FarmaciaLayout>
+    </>
+  );
+}
