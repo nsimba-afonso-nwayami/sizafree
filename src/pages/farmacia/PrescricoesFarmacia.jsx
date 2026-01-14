e2e17ed70c972caa4a7e0cf63aa9fcc4d301cf63
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 14:25:24 +0100

    Páginaprescrições da farmácia criada

---
+++ src/pages/farmacia/PrescricoesFarmacia.jsx
@@ -0,0 +1,11 @@
+import FarmaciaLayout from "./components/FarmaciaLayout";
+
+export default function PrescricoesFarmacia() {
+  return (
+    <>
+      <title>Solicitações | Dashboard Farmácia</title>
+
+      <FarmaciaLayout title="Solicitações"></FarmaciaLayout>
+    </>
+  );
+}
