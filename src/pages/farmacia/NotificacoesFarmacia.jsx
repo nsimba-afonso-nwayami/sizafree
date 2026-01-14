6531a0b80238b06c266d9740b01acf175f47f2ce
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 15:40:11 +0100

    Página de notificações da farmácia criada

---
+++ src/pages/farmacia/NotificacoesFarmacia.jsx
@@ -0,0 +1,11 @@
+import FarmaciaLayout from "./components/FarmaciaLayout";
+
+export default function NotificacoesFarmacia() {
+  return (
+    <>
+      <title>Notificações | Dashboard Farmácia</title>
+
+      <FarmaciaLayout title="Notificações"></FarmaciaLayout>
+    </>
+  );
+}
