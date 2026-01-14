5c5a6ce9baa4186634cbdd3e2f3c69ee05274b97
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 10:55:38 +0100

    Página dashboard da farmácia criada

---
+++ src/pages/farmacia/DashboardFarmacia.jsx
@@ -0,0 +1,12 @@
+import FarmaciaLayout from "./components/FarmaciaLayout";
+import { Link } from "react-router-dom";
+
+export default function DashboardFarmacia() {
+  return (
+    <>
+      <title>Dashboard Farmácia</title>
+      <FarmaciaLayout title="Dashboard Farmácia">
+      </FarmaciaLayout>
+    </>
+  );
+}
