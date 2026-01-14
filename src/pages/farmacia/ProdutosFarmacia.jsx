c624a99d6fc5eab5ef28df3e56eec91081378cff
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 15:40:55 +0100

    Página de produtos da farmácia criada

---
+++ src/pages/farmacia/ProdutosFarmacia.jsx
@@ -0,0 +1,11 @@
+import FarmaciaLayout from "./components/FarmaciaLayout";
+
+export default function ProdutosFarmacia() {
+  return (
+    <>
+      <title>Produtos | Dashboard Farmácia</title>
+
+      <FarmaciaLayout title="Produtos"></FarmaciaLayout>
+    </>
+  );
+}
