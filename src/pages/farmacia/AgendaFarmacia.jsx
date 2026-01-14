15c101bde3ee35b8e002445d5c50baecc5b9cbed
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 14:24:55 +0100

    Página agenda da farmácia criada

---
+++ src/pages/farmacia/AgendaFarmacia.jsx
@@ -0,0 +1,11 @@
+import FarmaciaLayout from "./components/FarmaciaLayout";
+
+export default function AgendaFarmacia() {
+  return (
+    <>
+      <title>Agenda de Atendimentos | Dashboard Farmácia</title>
+
+      <FarmaciaLayout title="Agenda de Atendimentos"></FarmaciaLayout>
+    </>
+  );
+}
