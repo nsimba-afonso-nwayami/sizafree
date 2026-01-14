7d36610e2a393b7edd0ca8be6beeb213ba2e71db
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 07:59:32 +0100

    Página relatórios do hospital criada

---
+++ src/pages/hospital/RelatoriosHospital.jsx
@@ -0,0 +1,11 @@
+import HospitalLayout from "./components/HospitalLayout";
+
+export default function RelatoriossHospital() {
+  return (
+    <>
+      <title>Relatórios | Dashboard Hospital</title>
+
+      <HospitalLayout title="Relatórios"></HospitalLayout>
+    </>
+  );
+}
