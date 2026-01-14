b2c2ec35bb3df1e0a0d1d0b6adda9078a8e6bfc5
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 07:55:45 +0100

    Página configurações do hospital criada

---
+++ src/pages/hospital/ConfiguracoesHospital.jsx
@@ -0,0 +1,11 @@
+import HospitalLayout from "./components/HospitalLayout";
+
+export default function ConfiguracoesHospital() {
+  return (
+    <>
+      <title>Configurações | Dashboard Hospital</title>
+
+      <HospitalLayout title="Configurações"></HospitalLayout>
+    </>
+  );
+}
