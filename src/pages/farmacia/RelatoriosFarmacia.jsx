c158edc5edef90c9bdeea75dc03e017ecabe01a8
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 15:41:19 +0100

    Página de relatórios da farmácia criada

---
+++ src/pages/farmacia/RelatoriosFarmacia.jsx
@@ -0,0 +1,11 @@
+import FarmaciaLayout from "./components/FarmaciaLayout";
+
+export default function RelatoriosFarmacia() {
+  return (
+    <>
+      <title>Relatórios Online | Dashboard Farmácia</title>
+
+      <FarmaciaLayout title="Relatórios"></FarmaciaLayout>
+    </>
+  );
+}
