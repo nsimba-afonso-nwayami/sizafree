28accdbfd9698bc4f9b071653e8e63ed5da3679e
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 07:55:30 +0100

    Página agenda do hospital criada

---
+++ src/pages/hospital/AgendaHospital.jsx
@@ -0,0 +1,14 @@
+import HospitalLayout from "./components/HospitalLayout";
+
+export default function AgendaHospital() {
+
+  return (
+    <>
+      <title>Agenda de Consultas | Dashboard Hospital</title>
+
+      <HospitalLayout title="Agenda de Consultas">
+        
+      </HospitalLayout>
+    </>
+  );
+}
