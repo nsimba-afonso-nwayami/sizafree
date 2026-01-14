f8bf589204ca561742d43930ce5671688454dee3
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 12:49:34 +0100

    Página de profissionais criada

---
+++ src/pages/farmacia/ProfissionaisFarmacia.jsx
@@ -0,0 +1,13 @@
+import FarmaciaLayout from "./components/FarmaciaLayout";
+import { Link } from "react-router-dom";
+
+export default function ProfissionaisFarmacia() {
+  return (
+    <>
+      <title>Profissinais | Dashboard Farmácia</title>
+      <FarmaciaLayout title="Profissinais">
+        
+      </FarmaciaLayout>
+    </>
+  );
+}
