201448e82e08acd127a34f8e1022d0611905fed5
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 15:40:30 +0100

    Página de pedidos da farmácia criada

---
+++ src/pages/farmacia/PedidosFarmacia.jsx
@@ -0,0 +1,11 @@
+import FarmaciaLayout from "./components/FarmaciaLayout";
+
+export default function PedidosFarmacia() {
+  return (
+    <>
+      <title>Pedidos Online | Dashboard Farmácia</title>
+
+      <FarmaciaLayout title="Pedidos Online"></FarmaciaLayout>
+    </>
+  );
+}
