5bd74b0ce1b63422921642b78b69ed3fe8d0952a
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 07:58:53 +0100

    Página prescrições do hospital criada

---
+++ src/pages/hospital/PrescricoesHospital.jsx
@@ -0,0 +1,11 @@
+import HospitalLayout from "./components/HospitalLayout";
+
+export default function PrescricoesHospital() {
+  return (
+    <>
+      <title>Prescrições | Dashboard Hospital</title>
+
+      <HospitalLayout title="Prescrições"></HospitalLayout>
+    </>
+  );
+}
