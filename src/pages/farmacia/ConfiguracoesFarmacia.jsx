8fe6917d36460c0af4e1bfd94452950759c8f62b
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 15:38:33 +0100

    Página de configurações da farmácia criada

---
+++ src/pages/farmacia/ConfiguracoesFarmacia.jsx
@@ -0,0 +1,11 @@
+import FarmaciaLayout from "./components/FarmaciaLayout";
+
+export default function ConfiguracoesFarmacia() {
+  return (
+    <>
+      <title>Configurações | Dashboard Farmácia</title>
+
+      <FarmaciaLayout title="Configurações"></FarmaciaLayout>
+    </>
+  );
+}
