de98ba3eb7583ef2b67c5f9829946a7c9ce073e3
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 07:59:10 +0100

    Página profissinais do hospital criada

---
+++ src/pages/hospital/ProfissionaisHospital.jsx
@@ -0,0 +1,11 @@
+import HospitalLayout from "./components/HospitalLayout";
+
+export default function ProfissionaisHospital() {
+  return (
+    <>
+      <title>Profissionais | Dashboard Hospital</title>
+
+      <HospitalLayout title="Profissionais"></HospitalLayout>
+    </>
+  );
+}
