a815849c7e4561b3b8aebd4ac07218ba40173e3c
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 14:25:09 +0100

    Página histórico da farmácia criada

---
+++ src/pages/farmacia/HistoricoFarmacia.jsx
@@ -0,0 +1,14 @@
+import FarmaciaLayout from "./components/FarmaciaLayout";
+
+export default function HistoricoFarmacia() {
+
+  return (
+    <>
+      <title>Histórico | Dashboard Farmácia</title>
+
+      <FarmaciaLayout title="Pacientes">
+        
+      </FarmaciaLayout>
+    </>
+  );
+}
