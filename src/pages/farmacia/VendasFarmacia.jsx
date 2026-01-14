cfb601f5a7fddd8e09e6b828b35573a3ff7070de
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 15:41:46 +0100

    Página de vendas da farmácia criada

---
+++ src/pages/farmacia/VendasFarmacia.jsx
@@ -0,0 +1,11 @@
+import FarmaciaLayout from "./components/FarmaciaLayout";
+
+export default function VendasFarmacia() {
+  return (
+    <>
+      <title>Vendas | Dashboard Farmácia</title>
+
+      <FarmaciaLayout title="Vendass"></FarmaciaLayout>
+    </>
+  );
+}
