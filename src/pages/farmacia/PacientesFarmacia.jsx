b7fc06df593c9ff2f77f0bfa5cb437fb7c36a1bf
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 13:33:57 +0100

    Página pacientes da farmácia criada

---
+++ src/pages/farmacia/PacientesFarmacia.jsx
@@ -0,0 +1,14 @@
+import FarmaciaLayout from "./components/FarmaciaLayout";
+
+export default function ProfissionaisFarmacia() {
+
+  return (
+    <>
+      <title>Pacientes | Dashboard Farmácia</title>
+
+      <FarmaciaLayout title="Pacientes">
+        
+      </FarmaciaLayout>
+    </>
+  );
+}
