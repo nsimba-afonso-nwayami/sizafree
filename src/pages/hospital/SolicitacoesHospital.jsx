7271290cf69ba2ee7073b0c37870f465c2b0b3c0
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 07:59:46 +0100

    Página solicitações do hospital criada

---
+++ src/pages/hospital/SolicitacoesHospital.jsx
@@ -0,0 +1,11 @@
+import HospitalLayout from "./components/HospitalLayout";
+
+export default function SolicitacoesHospital() {
+  return (
+    <>
+      <title>Solicitações de Consulta | Dashboard Hospital</title>
+
+      <HospitalLayout title="Solicitações de Consulta"></HospitalLayout>
+    </>
+  );
+}
