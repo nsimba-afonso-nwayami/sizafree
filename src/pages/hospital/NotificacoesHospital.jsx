e86258edfc44437ecb69fcea4cfba7a1c8464b12
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-14 07:58:05 +0100

    Página Notificações do hospital criada

---
+++ src/pages/hospital/NotificacoesHospital.jsx
@@ -0,0 +1,11 @@
+import HospitalLayout from "./components/HospitalLayout";
+
+export default function NotificacoesHospital() {
+  return (
+    <>
+      <title>Notificações | Dashboard Hospital</title>
+
+      <HospitalLayout title="Notificações"></HospitalLayout>
+    </>
+  );
+}
