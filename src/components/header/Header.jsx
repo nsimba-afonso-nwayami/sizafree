971c44356281c492c09d9ed8df588de4b1d52be6
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-15 09:55:46 +0100

    Alteração pontual

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,7 +11,6 @@ export default function Header() {
         <Link to="/" className="flex items-center gap-2 text-2xl font-bold">
           <i className="fa-solid fa-heart-pulse text-teal-500"></i>
           <span className="text-teal-800">Sisa</span>
-          <span className="text-teal-500">Free</span>
         </Link>
 
         {/* Botão mobile */}
