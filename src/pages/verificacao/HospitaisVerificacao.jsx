b09ff88e8af8762a1cccf519bb3ec84028ed381b
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-16 14:51:15 +0100

    Página de verificação de hospitais criada

---
+++ src/pages/verificacao/HospitaisVerificacao.jsx
@@ -0,0 +1,11 @@
+import VerificacaoLayout from "./components/VerificacaoLayout";
+import { Link } from "react-router-dom";
+
+export default function HospitaisVerificacao() {
+  return (
+    <>
+      <title>Hospitais | Dashboard Verificação</title>
+      <VerificacaoLayout title="Verificação de Hospitais"></VerificacaoLayout>
+    </>
+  );
+}
