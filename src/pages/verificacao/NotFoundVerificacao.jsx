5fe5afa558cf2cada6f9227899597eb2e4393514
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-16 14:26:38 +0100

    Página not found do dashboard verificação feita

---
+++ src/pages/verificacao/NotFoundVerificacao.jsx
@@ -0,0 +1,13 @@
+import VerificacaoLayout from "./components/VerificacaoLayout";
+import { Link } from "react-router-dom";
+
+export default function NotFoundVerificacao() {
+  return (
+    <>
+      <title>Página não encontrada | Dashboard Verificação</title>
+      <VerificacaoLayout title="Página não encontrada">
+       
+      </VerificacaoLayout>
+    </>
+  );
+}
